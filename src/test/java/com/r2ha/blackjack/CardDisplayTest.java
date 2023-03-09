4e49523fa9ef0166b3a1532b4d0c46c60774ee61
Daniel Ranner <daniel.ranner@jollydays.com>
2023-03-09 23:18:27 +0100

    Added characterization tests for Ten of Hearts and Five of Spades

---
+++ src/test/java/com/r2ha/blackjack/CardDisplayTest.java
@@ -0,0 +1,28 @@
+package com.r2ha.blackjack;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class CardDisplayTest {
+
+    @Test
+    void displaysTenCard() {
+        Card card = new Card(Suit.HEARTS, Rank.TEN);
+
+        String output = card.display();
+
+        assertThat(output)
+                .isEqualTo("[31m┌─────────┐[1B[11D│10       │[1B[11D│         │[1B[11D│    ♥    │[1B[11D│         │[1B[11D│       10│[1B[11D└─────────┘");
+    }
+
+    @Test
+    void displaysFiveCard() {
+        Card card = new Card(Suit.SPADES, Rank.FIVE);
+
+        String output = card.display();
+
+        assertThat(output)
+                .isEqualTo("[30m┌─────────┐[1B[11D│5        │[1B[11D│         │[1B[11D│    ♠    │[1B[11D│         │[1B[11D│        5│[1B[11D└─────────┘");
+    }
+}
